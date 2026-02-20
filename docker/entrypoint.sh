68e2f715dc0da1dccc9f8d10306f2712114a628f
Your Name <you@example.com>
2026-02-20 22:32:23 +0700

    add vendor dependency installation in entrypoint script if not present

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ ! -d "vendor" ] || [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-dev --optimize-autoloader --no-interaction
+fi
+
 php artisan optimize
 php artisan migrate --force
 php artisan storage:link 2>/dev/null || true
