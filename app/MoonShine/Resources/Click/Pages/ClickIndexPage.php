37414d84a297a0a3d40e0b4d6c016f39fb34172d
Your Name <you@example.com>
2026-02-21 20:54:31 +0700

    add referer and user agent fields to ClickIndexPage, update default.conf.template for SSL configuration

--- app/MoonShine/Resources/Click/Pages/ClickIndexPage.php
+++ app/MoonShine/Resources/Click/Pages/ClickIndexPage.php
@@ -28,6 +28,8 @@ class ClickIndexPage extends IndexPage
             Text::make('ID клика', 'click_id'),
             Date::make('Время', 'ts_utc')->format('d.m.Y H:i:s')->sortable(),
             Text::make('IP', 'ip'),
+            Text::make('Referer', 'referer'),
+            Text::make('User Agent', 'user_agent'),
             Text::make('SubID', 'subid'),
             Text::make('YM UID', 'ym_uid'),
             Date::make('Отправлено в YM', 'ym_sent_at')->format('d.m.Y H:i:s'),
